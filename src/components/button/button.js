b99060566eef91d4c85a392120235d4c01b74774
Logan Henrie <loghenrie@gmail.com>
2023-02-12 11:07:23 -0500

    Clean up index.html

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -4,7 +4,7 @@ function Button(props) {
 	return (
 		<div className="button">
 			<img src={props.icon} className="button__icon" />
-			<h4 className="button__text">{props.children}</h4>
+			<p className="button__text">{props.children}</p>
 		</div>
 	);
 }
