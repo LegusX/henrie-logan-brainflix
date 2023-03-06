237780ec3f923d04482556af5d600d15c9bf126c
Logan Henrie <loghenrie@gmail.com>
2023-03-06 01:19:28 -0500

    finish upload page

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,9 +1,14 @@
 import "./button.scss";
 
-function Button({ icon, children }) {
+function Button({ icon, children, inverted, click }) {
+	if (typeof icon === "undefined") icon = false;
+	if (typeof inverted === "undefined") inverted = false;
 	return (
-		<div className="button">
-			<img src={icon} className="button__icon" />
+		<div
+			className={`button${inverted ? "--inverted button" : ""}`}
+			onClick={click}
+		>
+			{icon && <img src={icon} className="button__icon" alt="" />}
 			<p className="button__text">{children}</p>
 		</div>
 	);
--- src/pages/upload/upload.js
+++ src/pages/upload/upload.js
@@ -1,5 +1,49 @@
 import "./upload.scss";
+import defaultImage from "../../assets/images/Upload-video-preview.jpg";
+import publishIcon from "../../assets/icons/publish.svg";
+
+import Button from "../../components/button/button";
+import { useNavigate } from "react-router-dom";
 
 export default function Upload() {
-	return <div></div>;
+	const navigate = useNavigate();
+
+	function publish() {
+		alert("Video Published!");
+		navigate("/");
+	}
+
+	return (
+		<div className="upload">
+			<h1 className="upload__title">Upload Video</h1>
+			<div className="upload__container">
+				<div className="upload__left">
+					<p className="upload__subtitle">Video Thumbnail</p>
+					<img src={defaultImage} className="upload__thumbnail" alt="" />
+				</div>
+				<div className="upload__right">
+					<div>
+						<p className="upload__subtitle">Title Your Video</p>
+						<input
+							className="upload__input"
+							placeholder="Add a title to your video"
+						/>
+					</div>
+					<div>
+						<p className="upload__subtitle">Add a video description</p>
+						<textarea
+							className="upload__input upload__textarea"
+							placeholder="Add a description to your video"
+						/>
+					</div>
+				</div>
+			</div>
+			<div className="upload__actions">
+				<Button icon={publishIcon} click={() => publish()}>
+					Publish
+				</Button>
+				<Button inverted={true}>Cancel</Button>
+			</div>
+		</div>
+	);
 }
